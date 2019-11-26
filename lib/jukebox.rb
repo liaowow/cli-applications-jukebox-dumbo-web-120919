73b60a2adb41dddb1c9c5642377f250b134d287e
Annie Liao <liaowow@yahoo.com>
2019-11-26 17:36:04 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,19 +47,21 @@ def run(songs)
   puts "Please enter a command:"
   input = gets.strip
   
-  while input != "exit" do
-    
-    if input == "help"
-      help
-    elsif input == "list"
+  case input
+    when "list"
       list(songs)
-    else
+    when "play"
       play(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox
+      break
+    else
+      help
     end
-    
   end
-  
-  exit_jukebox
+
 end
 
 ### For practice/fun ###
